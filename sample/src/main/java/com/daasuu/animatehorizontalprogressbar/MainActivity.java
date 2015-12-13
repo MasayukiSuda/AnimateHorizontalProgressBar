4f278e75d265bd70651b356d0af4322a0be83120
MasayukiSuda <sudamasayuki@suda-no-iMac.local>
2015-12-13 17:13:52 +0900

    update sample

--- sample/src/main/java/com/daasuu/animatehorizontalprogressbar/MainActivity.java
+++ sample/src/main/java/com/daasuu/animatehorizontalprogressbar/MainActivity.java
@@ -2,6 +2,7 @@ package com.daasuu.animatehorizontalprogressbar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.daasuu.library.AnimateHorizontalProgressBar;
 
@@ -14,8 +15,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.animate_progress_bar)
     AnimateHorizontalProgressBar mProgressBarAnimProgress;
-    @Bind(R.id.animate_max_progress_bar)
-    AnimateHorizontalProgressBar mProgressBarAnimMax;
+    @Bind(R.id.txt_progress)
+    TextView mTxtProgress;
+    @Bind(R.id.txt_max)
+    TextView mTxtMax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +26,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-
         mProgressBarAnimProgress.setMax(1000);
-        mProgressBarAnimProgress.setProgress(200);
-        mProgressBarAnimMax.setMax(1000);
-        mProgressBarAnimMax.setProgress(800);
+        mProgressBarAnimProgress.setProgress(400);
+
+        mProgressBarAnimProgress.setAnimateProgressListener(new AnimateHorizontalProgressBar.AnimateProgressListener() {
+            @Override
+            public void onAnimationStart(int progress, int max) {
+                // do nothing
+            }
+
+            @Override
+            public void onAnimationEnd(int progress, int max) {
+                mTxtProgress.setText(getString(R.string.progress, progress));
+                mTxtMax.setText(getString(R.string.max, max));
+            }
+        });
+
     }
 
-    @OnClick(R.id.btn_progress_anim)
-    public void onClickProgress() {
+    @OnClick(R.id.btn_progress_anim_0)
+    public void onClickProgress0() {
         mProgressBarAnimProgress.setProgressWithAnim(800);
     }
 
-    @OnClick(R.id.btn_max_anim)
-    public void onClickMax() {
-        mProgressBarAnimMax.setMaxWithAnim(2000);
+    @OnClick(R.id.btn_progress_anim_1)
+    public void onClickProgress1() {
+        mProgressBarAnimProgress.setProgressWithAnim(400);
+    }
+
+    @OnClick(R.id.btn_max_anim_0)
+    public void onClickMax0() {
+        mProgressBarAnimProgress.setMaxWithAnim(2000);
+    }
+
+    @OnClick(R.id.btn_max_anim_1)
+    public void onClickMax1() {
+        mProgressBarAnimProgress.setMaxWithAnim(3000);
+    }
+
+    @OnClick(R.id.btn_max_anim_2)
+    public void onClickMax2() {
+        mProgressBarAnimProgress.setMaxWithAnim(1000);
     }
 
 }
